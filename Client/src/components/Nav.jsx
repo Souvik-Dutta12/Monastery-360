2c5f90bb3dbad6617dfc031dcf2085f2d91fdb1b
Souvik Dutta <souvikdutta5122005@gmail.com>
2025-09-08 09:31:04 +0530

    hero1

--- Client/src/components/Nav.jsx
+++ Client/src/components/Nav.jsx
@@ -3,18 +3,29 @@ import logo from '../assets/Logo.png'
 import { Link } from 'react-router-dom'
 const Nav = () => {
   return (
-    <div className='w-full h-20 bg-amber-300 flex justify-evenly items-center text-2xl font-bold'>
-      <div className=' h-full '>
-        <img src={logo} alt="logo" className=' h-full'/>
+    <div className='fixed z-100 w-full h-20 backdrop-blur flex justify-between  px-10 pt-2 items-center text-2xl font-bold '>
+      <div className=' h-full flex items-center justify-center cursor-pointer'>
+        <Link to={'/'} className='h-full flex items-center justify-center'>
+          <img src={logo} alt="logo" className=' h-full' />
+          <div className='prata text-4xl text-red-900 text-shadow-lg'>Monastery 360</div>
+        </Link>
       </div>
-      <div>
-        <li>
-            <ul>
-                <Link to={'#'}></Link>
-            </ul>
-        </li>
+      <div className='flex justify-between w-auto  gap-70'>
+        <div className='flex gap-10  w-auto items-center'>
+          <span><Link to={'#'} className='prata text-red-950 text-shadow-md'>Home</Link></span>
+          <span><Link to={'#'} className='prata text-red-950 text-shadow-md'>Explore</Link></span>
+          <span><Link to={'#'} className='prata text-red-950 text-shadow-md'>Events</Link></span>
+          <span><Link to={'#'} className='prata text-red-950 text-shadow-md'>Digital Archives</Link></span>
+        </div>
+        <div className='flex gap-5'>
+          <button className='px-5 py-2 text-center bg-red-900 hover:bg-red-800 duration-300 text-amber-100 rounded-lg border cursor-pointer'>
+            <Link to={'#'} className='prata '>Login</Link>
+          </button>
+          <button className='px-5 py-2 text-center bg-amber-100 hover:bg-amber-200 duration-300 text-red-900 rounded-lg border cursor-pointer'>
+            <Link to={'#'} className='prata '>Sign up</Link>
+          </button>
+        </div>
       </div>
-      <div></div>
     </div>
   )
 }
--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import bg from '../assets/bg2.jpg'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>
+    <div className=' w-full h-full ' >
+      <div className='relative w-full h-screen '>
         <Nav />
-      Home
+        <div className='w-screen h-screen  absolute opacity-50' >
+          <img src={bg} alt="" className='w-full h-full object-cover' />
+        </div>
+
+        <div className='absolute w-screen h-screen flex flex-col items-center justify-center gap-2' >
+          <h1 className='text-8xl prata font-black text-red-900 text-shadow-lg'>Monastery <span className='text-red-800 text-shadow-lg'>360</span></h1>
+          <p className='text-5xl text-shadow-lg font-semibold text-red-950'>From Ancient Walls to digital worlds</p>
+          <p className='text-5xl text-shadow-lg font-semibold text-red-950'>A journey into</p>
+          <p className='text-7xl text-shadow-lg font-semibold text-red-950'><span className='prata text-red-800'>SIKKIM'S</span> Living Heritage</p>
+          <div className='flex gap-5 mt-15'>
+            <button className='px-5 py-2 text-center bg-red-900 hover:bg-red-800 duration-300 text-amber-100 rounded-lg border cursor-pointer'>
+              <Link to={'#'} className='prata text-2xl flex items-center justify-center gap-2'>Start Virtual Tour<i className="ri-arrow-right-line"></i></Link>
+            </button>
+            <button className='px-5 py-2 text-center bg-amber-100 hover:bg-amber-200 duration-300 text-red-900 rounded-lg border cursor-pointer'>
+              <Link to={'#'} className='prata text-2xl'>Book Your Visit</Link>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className='w-full min-h-screen'>
+
+      </div>
+
+
     </div>
   )
 }
