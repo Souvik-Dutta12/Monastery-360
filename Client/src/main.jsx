8259e0016a4371ef8573ad8071531715afcbffc1
Souvik Dutta <souvikdutta5122005@gmail.com>
2025-09-12 09:11:33 +0530

    change

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -15,9 +15,7 @@ createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <ClerkProvider 
       publishableKey={PUBLISHABLE_KEY}
-      localization={{
-        locale: 'en-IN'
-      }}
+      
     >
     <App />
     </ClerkProvider>
